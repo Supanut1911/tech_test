390e27d4ce46c03a7315001cf8e6598bd944e3db
supanut1911 <ynotmela@gmail.com>
2020-02-21 18:21:26 +0700

    Initial Commit

---
+++ techTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  techTest
+//
+//  Created by Supanut Laddayam on 21/2/2563 BE.
+//  Copyright © 2563 Supanut Laddayam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
